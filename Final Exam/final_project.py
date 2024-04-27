043883fd1359f041bbc070e7bcf7d0de34b9e19d
JobayerProdhan <167546934+JobayerProdhan@users.noreply.github.com>
2024-04-27 22:22:55 +0600

    Update final_project.py

--- Final Exam/final_project.py
+++ Final Exam/final_project.py
@@ -159,48 +159,55 @@ while True:
             else:
                 print("Invalid choice!")
     elif choice == 2:
+        name = input("Enter admin name: ") 
+        password = int(input("Enter your password:  "))
+        
+        if name.lower() == 'admin' and password == 12345:
+
         # Admin
-        admin = Admin(Bank())
-        while True:
-            print("****<<Admin's Options>>****")
-            print("1. Create an Account")
-            print("2. Delete an Account")
-            print("3. View all User Accounts")
-            print("4. Check Total Balance")
-            print("5. Check Total Loan Amount")
-            print("6. Toggle Loan Feature")
-            print("0. Exit from Admin's Menu")
+            admin = Admin(Bank())
+            while True:
+                print("****<<Admin's Options>>****")
+                print("1. Create an Account")
+                print("2. Delete an Account")
+                print("3. View all User Accounts")
+                print("4. Check Total Balance")
+                print("5. Check Total Loan Amount")
+                print("6. Toggle Loan Feature")
+                print("0. Exit from Admin's Menu")
 
-            admin_choice = int(input("Enter your choice: "))
+                admin_choice = int(input("Enter your choice: "))
 
-            if admin_choice == 1:
-                user_name = input("Enter User's Name: ")
-                user_address = input("Enter User's Address: ")
-                user_email = input("Enter User's Email Address: ")
-                user_account_type = input("Enter User's Account Type:\n1. Saving\n2. Current\t")
-                admin.create_account(user_name, user_email, user_address, user_account_type)
-            elif admin_choice == 2:
-                user_name = input("Enter User's Name to delete: ")
-                admin.delete_account(user_name)
-            elif admin_choice == 3:
-                admin.list_all_accounts()
-            elif admin_choice == 4:
-                admin.check_total_balance()
-            elif admin_choice == 5:
-                admin.check_total_loan_amount()
-            elif admin_choice == 6:
-                status = input("Enter 'on' to Enable Loan Feature or 'off' to disable: ")
-                if status.lower() == 'on':
-                    admin.toggle_loan_feature(True)
-                elif status.lower() == 'off':
-                    admin.toggle_loan_feature(False)
+                if admin_choice == 1:
+                    user_name = input("Enter User's Name: ")
+                    user_address = input("Enter User's Address: ")
+                    user_email = input("Enter User's Email Address: ")
+                    user_account_type = input("Enter User's Account Type:\n1. Saving\n2. Current\t")
+                    admin.create_account(user_name, user_email, user_address, user_account_type)
+                elif admin_choice == 2:
+                    user_name = input("Enter User's Name to delete: ")
+                    admin.delete_account(user_name)
+                elif admin_choice == 3:
+                    admin.list_all_accounts()
+                elif admin_choice == 4:
+                    admin.check_total_balance()
+                elif admin_choice == 5:
+                    admin.check_total_loan_amount()
+                elif admin_choice == 6:
+                    status = input("Enter 'on' to Enable Loan Feature or 'off' to disable: ")
+                    if status.lower() == 'on':
+                        admin.toggle_loan_feature(True)
+                    elif status.lower() == 'off':
+                        admin.toggle_loan_feature(False)
+                    else:
+                        print("Invalid input!!")
+                elif admin_choice == 0:
+                    print("Thank you for using our system!")
+                    break
                 else:
-                    print("Invalid input!!")
-            elif admin_choice == 0:
-                print("Thank you for using our system!")
-                break
-            else:
-                print("Invalid choice!")
+                    print("Invalid choice!")
+        else:
+            print("Invalid admin name and password. Try again ! ")
     elif choice == 3:
         print("Thank you for using our system!")
         break
